9f365c683b3ac6da061e62371e5fcf03baf65137
Kyle Combes <kylecombes@gmail.com>
2016-10-16 19:11:23 -0400

    Started implementing radiation flow through windows.

--- variables.m
+++ variables.m
@@ -7,3 +7,6 @@ house_width = 2.6;  % Width of house (m)
 house_height = 4;   % Height of house (m)
 wall_height = 5/8 * house_height;
 roof_hypot = 3/8 * house_height * sqrt(2); % Assume 45 degree roof angle
+
+% solar intensity throughout day
+[times, solar_intensity] = get_solar_intensity();
