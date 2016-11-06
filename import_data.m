2db5adf0cb53982bb9f8623381267e74ecada5ff
Kyle Combes <kylecombes@gmail.com>
2016-11-05 20:17:15 -0400

    Model works. Switched to simulating for Santa Rosa, CA, rather than Needham due to inability of house to keep warm there.

--- import_data.m
+++ import_data.m
@@ -1,5 +1,5 @@
 function [months, days, halfHours, dni, dhi, temp] = import_data(year)
-    f = strcat('solar_data\1321407_42.25_-71.22_',num2str(year),'.csv');
+    f = strcat('solar_data\SantaRosaCA_',num2str(year),'.csv');
     data = importdata(f);
     months = data.data(:,2);
     days = data.data(:,3);
